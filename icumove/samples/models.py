a3132d303daf7216ff33e806f8eff3a9f2461928
Gene Blanchard <gblanchard4@gmail.com>
2015-07-09 12:19:53 -0500

    added self methods

--- icumove/samples/models.py
+++ icumove/samples/models.py
@@ -26,15 +26,26 @@ class Air(models.Model):
 	pump = models.IntegerField(choices=PUMP_CHOICES, verbose_name='Pump Number'),
 	side = models.CharField(max_length=1, choices=SIDE_CHOICES, verbose_name='Pump Side')
 
+	def __str__(self):
+		return str("A-{}-{}-{}-{}{}{}{}".format(self.tower, self.date, self.time, self.tower, self.pump, self.side, self.day))
+
+
+
 class Door(models.Model):
 	date = models.DateField(verbose_name="Sample Date"),
 	time = models.TimeField(verbose_name="Time of Sample"),
 	icu = models.CharField(max_length=1, choices=ICU_CHOICES, verbose_name='ICU Location')
 
+	def __str__(self):
+		return str("D-{}-{}-{}-{}DC{}".format(self.tower, self.date, self.time, self.tower, self.day))
+
 class Floor(models.Model):
 	date = models.DateField(verbose_name="Sample Date"),
 	time = models.TimeField(verbose_name="Time of Sample"),
 	icu = models.CharField(max_length=1, choices=ICU_CHOICES, verbose_name="ICU Location")
+
+	def __str__(self):
+		return str("F-{}-{}-{}-{}FC{}".format(self.tower, self.date, self.time, self.tower, self.day))
 
 class Stool(models.Model):
 	date = models.DateField(verbose_name="Sample Date"),
@@ -43,4 +54,5 @@ class Stool(models.Model):
 	room = models.CharField(max_length=4, verbose_name="Room Number")
 	emr = models.CharField(max_length=10, verbose_name="Epic Medical Record Number)")
 
-
+	def __str__(self):
+		return str("A-{}-{}-{}-{}".format(self.tower, self.date, self.time, self.room))
