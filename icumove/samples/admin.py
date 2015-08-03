ff71d0cc88ca28f27976c653794d7bb05992ae43
Gene Blanchard <gblanchard4@gmail.com>
2015-08-03 11:50:39 -0500

    reorganize enviro

--- icumove/samples/admin.py
+++ icumove/samples/admin.py
@@ -45,14 +45,14 @@ class EnvironmentAdmin(admin.ModelAdmin):
 		('Date',{
 			'fields':['sample_date', 'time']
 		}),
-		('Tower 1',{
-			'fields':['tower1_E_temp','tower1_E_humi','tower1_E_pres','tower1_A_temp','tower1_A_humi','tower1_A_pres','tower1_B_temp','tower1_B_humi','tower1_B_pres','tower1_C_temp','tower1_C_humi','tower1_C_pres']
+		('Tower 1:',{
+			'fields':[('tower1_E_temp','tower1_E_humi','tower1_E_pres'),('tower1_A_temp','tower1_A_humi','tower1_A_pres'),('tower1_B_temp','tower1_B_humi','tower1_B_pres'),('tower1_C_temp','tower1_C_humi','tower1_C_pres')]
 		}),
 		('Tower 2',{
-			'fields':['tower2_E_temp','tower2_E_humi','tower2_E_pres','tower2_A_temp','tower2_A_humi','tower2_A_pres','tower2_B_temp','tower2_B_humi','tower2_B_pres','tower2_C_temp','tower2_C_humi','tower2_C_pres']
+			'fields':[('tower2_E_temp','tower2_E_humi','tower2_E_pres'),('tower2_A_temp','tower2_A_humi','tower2_A_pres'),('tower2_B_temp','tower2_B_humi','tower2_B_pres'),('tower2_C_temp','tower2_C_humi','tower2_C_pres')]
 		}),
 		('Tower 3',{
-			'fields':['tower3_A_temp','tower3_A_humi','tower3_A_pres','tower3_B_temp','tower3_B_humi','tower3_B_pres']
+			'fields':[('tower3_A_temp','tower3_A_humi','tower3_A_pres'),('tower3_B_temp','tower3_B_humi','tower3_B_pres')]
 		})
 	)
 
